e114f70cb3d6d61ab8d27540a6c08c7710de4c30
Your Name <you@example.com>
2023-11-26 20:52:44 +0530

    StateNotifireUsingCountExample

--- lib/Screen/Homepage.dart
+++ lib/Screen/Homepage.dart
@@ -7,65 +7,75 @@ final CounterStateProvider = StateProvider<int>((ref) {
 });
 
 class homepage extends ConsumerWidget {
-  void submit(value, WidgetRef ref) {
-    ref.read(userProvider.notifier).updateName(value);
-  }
+  // void submit(value, WidgetRef ref) {
+  //   ref.read(userProvider.notifier).updateName(value);
+  // }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     print("I am build in riverpod");
-    final userInfo = ref.watch(userProvider);
+
+    // final userInfo = ref.watch(userProvider);
     // final CountProvider = ref.watch(CounterStateProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Multi-Providerusing ex"),
       ),
-      body: Container(
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              onSubmitted: (val) {
-                submit(val, ref);
-              },
-              decoration: InputDecoration(
-                  hintText: "Enter your name", helperText: "Enter your name"),
-            ),
-            TextField(
-              onSubmitted: (value) {
-                ref.read(userProvider.notifier).updateAge(int.parse(value));
-              },
-              decoration: InputDecoration(
-                  hintText: "Enter your name", helperText: "Enter your name"),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  child: Text(userInfo.name),
-                  height: 50,
-                  width: 100,
-                  decoration: BoxDecoration(border: Border.all(width: 3)),
-                ),
-                Container(
-                  child: Text(userInfo.age.toString()),
-                  height: 50,
-                  width: 100,
-                  decoration: BoxDecoration(border: Border.all(width: 3)),
-                )
-              ],
-            )
-          ],
-        )),
-      ),
+      body: Container(child: Center(
+        child: Consumer(
+          builder: (contex, ref, child) {
+            final num = ref.watch(countProvider);
+
+            return Text(num.toString());
+          },
+
+          //     child: Column(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: [
+          //     TextField(
+          //       onSubmitted: (val) {
+          //         submit(val, ref);
+          //       },
+          //       decoration: InputDecoration(
+          //           hintText: "Enter your name", helperText: "Enter your name"),
+          //     ),
+          //     TextField(
+          //       onSubmitted: (value) {
+          //         ref.read(userProvider.notifier).updateAge(int.parse(value));
+          //       },
+          //       decoration: InputDecoration(
+          //           hintText: "Enter your name", helperText: "Enter your name"),
+          //     ),
+          //     SizedBox(
+          //       height: 20,
+          //     ),
+          //     Row(
+          //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //       children: [
+          //         Container(
+          //           child: Text(userInfo.name),
+          //           height: 50,
+          //           width: 100,
+          //           decoration: BoxDecoration(border: Border.all(width: 3)),
+          //         ),
+          //         Container(
+          //           child: Text(userInfo.age.toString()),
+          //           height: 50,
+          //           width: 100,
+          //           decoration: BoxDecoration(border: Border.all(width: 3)),
+          //         )
+          //       ],
+          //     )
+          //   ],
+          // ),
+        ),
+      )),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.send),
+        onPressed: () {
+          ref.read(countProvider.notifier).increament();
+        },
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
--- lib/Screen/class.dart
+++ lib/Screen/class.dart
@@ -1,71 +1,86 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
+// // ignore_for_file: public_member_api_docs, sort_constructors_first
+// import 'dart:convert';
+
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// final userProvider = StateNotifierProvider<userNotifier, user>(
+//     (ref) => userNotifier(user(name: "", age: 0)));
+
+// class user {
+//   final String name;
+//   final int age;
+
+//   user({
+//     required this.name,
+//     required this.age,
+//   });
+
+//   user copyWith({
+//     String? name,
+//     int? age,
+//   }) {
+//     return user(
+//       name: name ?? this.name,
+//       age: age ?? this.age,
+//     );
+//   }
+
+//   Map<String, dynamic> toMap() {
+//     return <String, dynamic>{
+//       'name': name,
+//       'age': age,
+//     };
+//   }
+
+//   factory user.fromMap(Map<String, dynamic> map) {
+//     return user(
+//       name: map['name'] as String,
+//       age: map['age'] as int,
+//     );
+//   }
+
+//   String toJson() => json.encode(toMap());
+
+//   factory user.fromJson(String source) =>
+//       user.fromMap(json.decode(source) as Map<String, dynamic>);
+
+//   @override
+//   String toString() => 'user(name: $name, age: $age)';
+
+//   @override
+//   bool operator ==(covariant user other) {
+//     if (identical(this, other)) return true;
+
+//     return other.name == name && other.age == age;
+//   }
+
+//   @override
+//   int get hashCode => name.hashCode ^ age.hashCode;
+// }
+
+// class userNotifier extends StateNotifier<user> {
+//   userNotifier(super.state);
+
+//   void updateName(String n) {
+//     state = state.copyWith(name: n);
+//   }
+
+//   void updateAge(int a) {
+//     state = state.copyWith(age: a);
+//   }
+// }
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final userProvider = StateNotifierProvider<userNotifier, user>(
-    (ref) => userNotifier(user(name: "", age: 0)));
+final countProvider = StateNotifierProvider<CountNotifier, int>((ref) {
+  return CountNotifier(0);
+});
 
-class user {
-  final String name;
-  final int age;
+class CountNotifier extends StateNotifier<int> {
+  CountNotifier(super.state);
+  // CountNotifier() : super(0);
 
-  user({
-    required this.name,
-    required this.age,
-  });
-
-  user copyWith({
-    String? name,
-    int? age,
-  }) {
-    return user(
-      name: name ?? this.name,
-      age: age ?? this.age,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'name': name,
-      'age': age,
-    };
-  }
-
-  factory user.fromMap(Map<String, dynamic> map) {
-    return user(
-      name: map['name'] as String,
-      age: map['age'] as int,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory user.fromJson(String source) =>
-      user.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() => 'user(name: $name, age: $age)';
-
-  @override
-  bool operator ==(covariant user other) {
-    if (identical(this, other)) return true;
-
-    return other.name == name && other.age == age;
-  }
-
-  @override
-  int get hashCode => name.hashCode ^ age.hashCode;
-}
-
-class userNotifier extends StateNotifier<user> {
-  userNotifier(super.state);
-
-  void updateName(String n) {
-    state = state.copyWith(name: n);
-  }
-
-  void updateAge(int a) {
-    state = state.copyWith(age: a);
+  void increament() {
+    state = state + 1;
   }
 }
