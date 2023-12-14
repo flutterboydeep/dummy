59c4813c23295cfb22eee01d933580d88a3b0b59
Your Name <you@example.com>
2023-12-14 17:42:57 +0530

    streamProvider

--- lib/Screen/Homepage.dart
+++ lib/Screen/Homepage.dart
@@ -4,7 +4,7 @@ import 'dart:developer';
 import 'package:dummy/Screen/class.dart';
 import 'package:dummy/Screen/homepage.dart';
 import 'package:dummy/Screen/steamFetchData.dart';
-import 'package:dummy/main.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
